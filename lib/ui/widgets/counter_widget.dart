ef90d3de29a3f673a1c2ca5c539e72413faeb8cf
sonixkd <113375135+sonixkd@users.noreply.github.com>
2023-05-01 16:03:47 +0300

    add saved data shared_preferences

--- lib/ui/widgets/counter_widget.dart
+++ lib/ui/widgets/counter_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterWidget extends StatefulWidget {
   const CounterWidget({super.key});
@@ -10,6 +11,20 @@ class CounterWidget extends StatefulWidget {
 
 class _CounterWidgetState extends State<CounterWidget> {
   int age = 0;
+
+  void loadAge() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    age = sharedPreferences.getInt('age') ?? 0;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadAge();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +35,25 @@ class _CounterWidgetState extends State<CounterWidget> {
             children: [
               Text(age.toString()),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   age++;
+
+                  final sharedPreferences =
+                      await SharedPreferences.getInstance();
+                  sharedPreferences.setInt('age', age);
+
                   setState(() {});
                 },
                 child: Text('+'),
               ),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   age = max(age - 1, 0);
+
+                  final sharedPreferences =
+                      await SharedPreferences.getInstance();
+                  sharedPreferences.setInt('age', age);
+
                   setState(() {});
                 },
                 child: Text('-'),
